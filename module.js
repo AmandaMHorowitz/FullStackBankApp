c9db0295d63117d4cee3b946a73ccf9d9c9f6fc5
AmandaMHorowitz <horowitz.amandam@gmail.com>
2023-09-18 21:27:47 -0400

    Update module.js

--- module.js
+++ module.js
@@ -39,12 +39,12 @@ ui.createAccount = ' \
     <div class="card text-white bg-primary mb-3" style="max-width: 18rem;"> \
         <div class="card-header">Create Account</div> \
         <div class="card-body"> \
-            Name<br> \
-            <input type="input" class="form-control" id="name" placeholder="Enter Your Name"><br> \
-            Email Address<br> \
-            <input type="input" class="form-control" id="email" placeholder="Enter Your Email"><br> \
-            Password<br> \
-            <input type="password" class="form-control" id="password" placeholder="Enter Your Password"><br> \
+            Name<br/> \
+            <input type="input" class="form-control" id="name" placeholder="Enter name"><br/> \
+            Email Address<br/> \
+            <input type="input" class="form-control" id="email" placeholder="Enter email"><br/> \
+            Password<br/> \
+            <input type="password" class="form-control" id="password" placeholder="Enter password"><br/> \
             <button type="submit" id="submit" class="btn" onclick="create()">Create Account</button> \
             <div id="createStatus"></div> \
         </div> \
