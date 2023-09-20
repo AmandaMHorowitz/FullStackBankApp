b91945a28f3ed9c896f8aacadad322a18d73f04c
AmandaMHorowitz <horowitz.amandam@gmail.com>
2023-09-19 20:51:09 -0400

    Update home.js

--- home.js
+++ home.js
@@ -4,7 +4,7 @@ function Home(){
         txtcolor="black"
         header="Bank of Horowitz"
         title="Welcome to the bank"
-        text="Please use this site for all your banking needs. Security not guaranteed."
+        text="Please use this site for all your banking needs. Security now guaranteed."
         body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
       />    
     );  
