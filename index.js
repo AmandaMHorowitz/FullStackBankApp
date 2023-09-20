6ec3646883d708264fa4033e71913572d184184e
AmandaMHorowitz <horowitz.amandam@gmail.com>
2023-09-19 21:29:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ var express = require('express');
 var app     = express();
 var cors    = require('cors');
 
+// used to serve statuc files from public directory
+app.use(express.static('public'));
+app.use(cors());
+
+// create user account
+app.get('/account/create/:name/:email/:password', function (req, res) {
+    res.send({
+        name:       req.params.name
+        email:      req.params.email,
+        password:   req.params.password
+    });
+});
