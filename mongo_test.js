923868ff34f7a91a59eddae077dc3b7af9702252
AmandaMHorowitz <horowitz.amandam@gmail.com>
2023-09-21 19:30:09 -0400

    Update mongo_test.js

--- mongo_test.js
+++ mongo_test.js
@@ -2,7 +2,7 @@ const MongoClient = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017';
 
 MongoClient.connect(url, function(err, client) {
-    console.log('Connected!');
+    console.log('Connected!')
 
     // database Name
     // const dbName = 'myproject';
