162c35ccc04bd53a3ddc17856c34a47abda7cf82
AmandaMHorowitz <horowitz.amandam@gmail.com>
2023-09-22 14:58:13 -0400

    Update index.js

--- security/index.js
+++ security/index.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
