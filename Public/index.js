68e0c93e02a6714987382bf8a3e9d9cf364c2df1
AmandaMHorowitz <horowitz.amandam@gmail.com>
2023-09-22 14:07:17 -0400

    Update index.js

--- Public/index.js
+++ Public/index.js
@@ -5,7 +5,7 @@ function Spa() {
           <div>
             <hr/>
           </div>
-        <UserContext.Provider value={{users:[{name:'Horowitz',email:'Horowitz@gmail.com',password:'secret',balance:100}]}}>
+        <UserContext.Provider value={{users:[{name:'Horowitz',email:'Horowitz@gmail.com',password:'secret',balance:0}]}}>
           <div className="container" style={{padding: "20px"}}>
             <Route path="/" exact         component={Home} />
             <Route path="/CreateAccount/" component={CreateAccount} />
