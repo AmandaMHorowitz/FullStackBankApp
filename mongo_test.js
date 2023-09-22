05c277b8c5f7cad78c808b63be945fe2d0d40236
AmandaMHorowitz <horowitz.amandam@gmail.com>
2023-09-21 21:38:17 -0400

    Update mongo_test.js

--- mongo_test.js
+++ mongo_test.js
@@ -1,22 +1,38 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://127.0.0.1:27017/local';
+const uri = 'mongodb://127.0.0.1:27017';
+const client = new MongoClient(uri);
 
-MongoClient.connect(url, function(err, client) {
-    console.log('Connected!');
-    client.close();
+// Connect to the MongoDB cluster
+client.connect(function(err) {
+    if (err) {
+        console.error('Error connecting to MongoDB:', err);
+        return;
+    }
 
-    // database Name
-    // const dbName = 'myproject';
-    // const db = client.db(dbName);
+    console.log('Connected to MongoDB!');
+
+    //database Name
+    const dbName = 'myproject';
+    const db = client.db(dbName);
 
     //new user
-    //var name = 'user' + Math.floor(Math.random()*1000);
-    //var email = name + '@mit.edu';
+    var name = 'user' + Math.floor(Math.random()*10000);
+    var email = name + '@mit.edu';
 
     //insert into customer table
-    //var collection = db.collection('customers');
-    //var doc = {name, email};
-    //collection.insertOne(doc, {w:1}, function(err, result) {
-        //console.log('Document insert');
-    //});
+    var collection = db.collection('customers');
+    var doc = {name, email};
+    collection.insertOne(doc, {w:1}, function(err, result) {
+        console.log('Document insert');
+    });
+
+    var customers = db
+        .collection('customers')
+        .find()
+        .toArray(function(err, docs) {
+            console.log('Collection: ', docs);
+          
+            // clear up
+            client.close();
+        });
 });
